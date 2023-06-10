70470f71d670a8e34e5f070f4e4c09db03ff6917
Memory-Wabwile <memorywabwile@gmail.com>
2023-06-10 13:05:32 +0300

    install flask

---
+++ data.py
@@ -0,0 +1,32 @@
+
+def Articles(): 
+    # a variable called articles that will hold all the articles 
+    articles = [
+        {
+            # the articles will go in here
+            'id' : 1 ,
+            'title' : 'Article One',
+            'body' : 'The first Article.First, solve the problem. Then, write the code.',
+            'author' : 'John Johnson' ,
+            'create_date' : '04-25-2021'
+        },
+
+        {
+            # the second article goes here
+            'id' : 2 ,
+            'title' : 'Article Two',
+            'body' : 'The second Article.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta velit vero doloremque vel id a explicabo, nesciunt in dolor minus!',
+            'author' : 'M.J' ,
+            'create_date' : '04-25-2022'
+        },
+
+        {
+            # the second article goes here
+            'id' : 3 ,
+            'title' : 'Article three',
+            'body' : 'The second Article.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta velit vero doloremque vel id a explicabo, nesciunt in dolor minus!',
+            'author' : 'M.J' ,
+            'create_date' : '04-25-2022'
+        },
+    ]
+    return articles
