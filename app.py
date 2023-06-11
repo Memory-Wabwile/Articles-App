25ea7a177d5676c3ee5386205828fad886073221
Memory-Wabwile <memorywabwile@gmail.com>
2023-06-11 20:52:31 +0300

    add register and login buttons to the home page

--- app.py
+++ app.py
@@ -30,11 +30,33 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles = Articles)
+    # create cursor 
+    cur = mysql.connection.cursor()
 
+    # get articles 
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+       return render_template('articles.html', articles=articles) 
+    else:
+        msg= 'No articles Found'
+        return render_template('articles.html' , msg=msg)
+    # close the connection 
+    cur.close()
+    
+# single article 
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
+    # create cursor 
+    cur = mysql.connection.cursor()
+        
+    # get articles 
+    result = cur.execute("SELECT * FROM articles WHERE id=%s" , [id])
+
+    article = cur.fetchone()
+    return render_template('article.html', article=article)
 
 
 class RegisterForm(Form):
@@ -139,7 +161,21 @@ def logout():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    # create cursor 
+    cur = mysql.connection.cursor()
+
+    # get articles 
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+       return render_template('dashboard.html', articles=articles) 
+    else:
+        msg= 'No articles Found'
+        return render_template('dashboard.html' , msg=msg)
+    # close the connection 
+    cur.close()
 
 # Article Form class
 class ArticlesForm(Form):
